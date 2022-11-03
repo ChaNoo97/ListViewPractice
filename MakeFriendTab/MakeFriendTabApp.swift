3b73f0c2570a6e143ac8e8af0cf846f32c693bc9
HOO <hoo@burritowallet.com>
2022-11-03 19:28:57 +0900

    FirstCommit

---
+++ MakeFriendTab/MakeFriendTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeFriendTabApp.swift
+//  MakeFriendTab
+//
+//  Created by HOO on 2022/11/03.
+//
+
+import SwiftUI
+
+@main
+struct MakeFriendTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
